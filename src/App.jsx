e0c8e0e58a17f9750fa3504e4d421d8c59350089
Iulian Crisan <iuliancrisan01@gmail.com>
2019-11-14 16:10:12 +0200

    Themed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Div } from 'styled'
-import ThemeContext from './context/themeContext'
+import styled from 'styled-components'
+import { Navbar } from 'shared'
 
 const App = () => {
-  const {
-    actions: { triggerTheme }
-  } = useContext(ThemeContext)
-
   return (
-    <Div>
-      <Div>Hello</Div>
-      <Div>
-        <button onClick={() => triggerTheme()}>trigger</button>
-      </Div>
-    </Div>
+    // <Div>
+    //   <Div>Hello</Div>
+    //   <Div>
+    //     <button onClick={() => triggerTheme()}>trigger</button>
+    //   </Div>
+    // </Div>
+    <Page>
+      <Navbar />
+      <Content>Hello there</Content>
+    </Page>
   )
 }
 
 export default App
+
+const Page = styled(Div)`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const Content = styled(Div)`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  max-width: 1000px;
+  margin: 0 auto;
+`
