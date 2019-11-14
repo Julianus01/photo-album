db05c8e8ff1b29dafc00e3a7704bf1a170ea25a8
Iulian Crisan <iuliancrisan01@gmail.com>
2019-11-14 15:28:51 +0200

    Created theme context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { ThemeProvider as StyledThemeProvider } from 'styled-components'
+import {
+  ThemeContextConsumer,
+  ThemeContextProvider
+} from 'context/themeContext'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const vh = window.innerHeight * 0.01
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+const RootHTML = () => {
+  return (
+    <ThemeContextProvider>
+      <ThemeContextConsumer>
+        {({ theme }) => (
+          <StyledThemeProvider theme={theme}>
+            <App />
+          </StyledThemeProvider>
+        )}
+      </ThemeContextConsumer>
+    </ThemeContextProvider>
+  )
+}
+
+ReactDOM.render(<RootHTML />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
